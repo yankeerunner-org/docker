ff6d99156b2ae74672dadb4a99403bc0eb871715
Takeshi Fukui <yankeerunner@gmail.com>
2021-02-28 14:49:09 +0900

    Yocto作成環境新規追加

---
+++ yocto/build.sh
@@ -0,0 +1,17 @@
+git clone git://git.yoctoproject.org/poky
+cd /home/dev/poky
+git checkout origin/gatesgarth -b gatesgarth
+git clone git://git.yoctoproject.org/meta-raspberrypi
+cd /home/dev/poky/meta-raspberrypi/
+git checkout origin/gatesgarth -b gatesgarth
+cd /home/dev/poky
+git clone git://git.openembedded.org/meta-openembedded
+source oe-init-build-env rpi-build
+bitbake-layers add-layer ../meta-raspberrypi/
+bitbake-layers add-layer ../meta-openembedded/meta-oe
+bitbake-layers add-layer ../meta-openembedded/meta-python
+bitbake-layers add-layer ../meta-openembedded/meta-multimedia
+bitbake-layers add-layer ../meta-openembedded/meta-networking
+sed -i -e "s/qemux86-64/raspberrypi4-64/g" /home/dev/poky/rpi-build/conf/local.conf
+bitbake core-image-base
+cp /home/dev/poky/rpi-build/tmp/deploy/images/raspberrypi4-64/core-image-base-raspberrypi4-64.wic.bz2 /home/dev/deploy
---
+++ yocto/run.sh
@@ -0,0 +1,1 @@
+docker run --rm -it -v "$(pwd):/home/dev/deploy" -e TZ=Asia/Tokyo yocto:latest /bin/bash /home/dev/build.sh
