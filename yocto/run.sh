da1ef79fb36f259486b73e0b299092fbd1428d87
Takeshi Fukui <yankeerunner@gmail.com>
2021-03-06 14:10:44 +0900

    vim追加, containerが消えないように変更, setupとbuildを分割

--- yocto/run.sh
+++ yocto/run.sh
@@ -1,1 +1,1 @@
-docker run --rm -it -v "$(pwd):/home/dev/deploy" -e TZ=Asia/Tokyo yocto:latest /bin/bash /home/dev/build.sh
+docker run -it -v "$(pwd):/home/dev/deploy" -e TZ=Asia/Tokyo yocto:latest /bin/bash /home/dev/setup.sh
