cca092c8affbb8ef46190621098913a2cedf6b83
Takeshi Fukui <yankeerunner@gmail.com>
2022-01-31 22:08:39 +0900

    remove unnesessary steps

--- yocto/build.sh
+++ yocto/build.sh
@@ -4,6 +4,4 @@ echo 'SSTATE_DIR ?= "/home/dev/share/sstate-cache"' >> ./rpi-build/conf/local.co
 source oe-init-build-env rpi-build
 time bitbake core-image-base
 mkdir -p /home/dev/host/deploy/image
-cp /home/dev/poky/rpi-build/tmp/deploy/images/raspberrypi4-64/core-image-base-raspberrypi4-64.wic.bz2 /home/dev/host/deploy/image/core-image-base-raspberrypi4-64.wic.bz2 
-mkdir -p /home/dev/share/deploy/image
-cp /home/dev/poky/rpi-build/tmp/deploy/images/raspberrypi4-64/core-image-base-raspberrypi4-64.wic.bz2 /home/dev/share/deploy/image/core-image-base-raspberrypi4-64.wic.bz2 
+cp /home/dev/poky/rpi-build/tmp/deploy/images/raspberrypi4-64/core-image-base-raspberrypi4-64.wic.bz2 /home/dev/host/deploy/image/core-image-base-raspberrypi4-64.wic.bz2
