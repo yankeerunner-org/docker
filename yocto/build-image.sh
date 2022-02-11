f70812e0c53d5da499cde81c5334cc779d335871
Takeshi Fukui <yankeerunner@gmail.com>
2022-02-11 15:28:41 +0900

    fix docker pull

--- yocto/build-image.sh
+++ yocto/build-image.sh
@@ -1,2 +1,2 @@
-docker pull ghcr.io/yankeerunner_org/yocto:dunfell
-docker run --rm -v "$(cd $(dirname $0); pwd):/home/dev/host" -v "${HOME}/develop/yocto/share:/home/dev/share" -e TZ=Asia/Tokyo ghcr.io/yankeerunner_org/yocto:dunfell /bin/bash /home/dev/host/build.sh
+docker pull ghcr.io/yankeerunner-org/yocto:dunfell
+docker run --rm -v "$(cd $(dirname $0); pwd):/home/dev/host" -v "${HOME}/develop/yocto/share:/home/dev/share" -e TZ=Asia/Tokyo ghcr.io/yankeerunner-org/yocto:dunfell /bin/bash /home/dev/host/build.sh
