7bf205b2e647a372cc4d7d00bf52b6e4da625626
Takeshi Fukui <yankeerunner@gmail.com>
2022-02-11 13:56:46 +0900

    add pull command

--- yocto/build-image.sh
+++ yocto/build-image.sh
@@ -1,1 +1,2 @@
+docker pull ghcr.io/yankeerunner_org/yocto:dunfell
 docker run --rm -v "$(cd $(dirname $0); pwd):/home/dev/host" -v "${HOME}/develop/yocto/share:/home/dev/share" -e TZ=Asia/Tokyo ghcr.io/yankeerunner_org/yocto:dunfell /bin/bash /home/dev/host/build.sh
