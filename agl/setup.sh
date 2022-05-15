794d17737358e5d86b39dedc589d315656f485b6
Takeshi Fukui <yankeerunner@gmail.com>
2022-05-15 17:28:42 +0900

    環境変数引継ぎがうまく行かないのでsetup.shでbuild.sh相当も実施

--- agl/setup.sh
+++ agl/setup.sh
@@ -24,3 +24,15 @@ repo sync
 #cd master
 #repo init -u https://gerrit.automotivelinux.org/gerrit/AGL/AGL-repo
 #repo sync
+
+# setup build environment
+cd $AGL_TOP/koi
+
+# build setup
+source meta-agl/scripts/aglsetup.sh -f -m raspberrypi4 -b raspberrypi4 agl-demo agl-appfw-smack
+
+# if want to customise, please change /build/conf/local.conf file
+
+# build agl-demo-platform
+time bitbake agl-demo-platform
+cp -R $AGL_TOP/koi/raspberrypi4/tmp/deploy/images/raspberrypi4-64 /home/dev/share/agl
