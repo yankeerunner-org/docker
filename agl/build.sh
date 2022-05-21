c80b5407e6a9e6ff2f76a0964b6b0dae749cb553
Takeshi Fukui <yankeerunner@gmail.com>
2022-05-21 17:11:06 +0900

    update koi to marlin

--- agl/build.sh
+++ agl/build.sh
@@ -1,13 +1,13 @@
 # setup build environment
-cd $AGL_TOP/koi
+cd $AGL_TOP
 
 # build setup
 source meta-agl/scripts/aglsetup.sh -f -m raspberrypi4 -b raspberrypi4 agl-demo agl-appfw-smack
 
 # if want to customise, please change /build/conf/local.conf file
-echo 'DL_DIR ?= "/home/dev/share/agl/downloads"' >> $AGL_TOP/koi/raspberrypi4/conf/local.conf
-echo 'SSTATE_DIR ?= "/home/dev/share/agl/sstate-cache"' >> $AGL_TOP/koi/raspberrypi4/conf/local.conf
+echo 'DL_DIR ?= "/home/dev/share/agl/downloads"' >> $AGL_TOP/raspberrypi4/conf/local.conf
+echo 'SSTATE_DIR ?= "/home/dev/share/agl/sstate-cache"' >> $AGL_TOP/raspberrypi4/conf/local.conf
 
 # build agl-demo-platform
 time bitbake agl-demo-platform
-cp -R $AGL_TOP/koi/raspberrypi4/tmp/deploy/images/raspberrypi4-64 /home/dev/share/agl
+cp -R $AGL_TOP/raspberrypi4/tmp/deploy/images/raspberrypi4-64 /home/dev/share/agl
--- agl/setup.sh
+++ agl/setup.sh
@@ -13,28 +13,19 @@ chmod a+x $HOME/bin/repo
 # download the agl source files
 # stable release
 cd $AGL_TOP
-mkdir koi
-cd koi
-repo init -b koi -m koi_11.0.1.xml -u https://gerrit.automotivelinux.org/gerrit/AGL/AGL-repo
+repo init -b marlin -m marlin_13.0.0.xml -u https://gerrit.automotivelinux.org/gerrit/AGL/AGL-repo
 repo sync
 
-# cutting edge...
-#cd $AGL_TOP
-#mkdir master
-#cd master
-#repo init -u https://gerrit.automotivelinux.org/gerrit/AGL/AGL-repo
-#repo sync
-
 # setup build environment
-cd $AGL_TOP/koi
+cd $AGL_TOP
 
 # build setup
 source meta-agl/scripts/aglsetup.sh -f -m raspberrypi4 -b raspberrypi4 agl-demo agl-appfw-smack
 
 # if want to customise, please change /build/conf/local.conf file
-echo 'DL_DIR ?= "/home/dev/share/agl/downloads"' >> $AGL_TOP/koi/raspberrypi4/conf/local.conf
-echo 'SSTATE_DIR ?= "/home/dev/share/agl/sstate-cache"' >> $AGL_TOP/koi/raspberrypi4/conf/local.conf
+echo 'DL_DIR ?= "/home/dev/share/agl/downloads"' >> $AGL_TOP/raspberrypi4/conf/local.conf
+echo 'SSTATE_DIR ?= "/home/dev/share/agl/sstate-cache"' >> $AGL_TOP/raspberrypi4/conf/local.conf
 
 # build agl-demo-platform
 time bitbake agl-demo-platform
-cp -R $AGL_TOP/koi/raspberrypi4/tmp/deploy/images/raspberrypi4-64 /home/dev/share/agl
+cp -R $AGL_TOP/raspberrypi4/tmp/deploy/images/raspberrypi4-64 /home/dev/share/agl
