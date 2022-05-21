8d2a9d1d0c8d59dde9980e07f36d9bdb3a8bcb3e
Takeshi Fukui <yankeerunner@gmail.com>
2022-05-21 17:29:30 +0900

    agl-appfw-smackはmarlinだと使えない模様

--- agl/build.sh
+++ agl/build.sh
@@ -2,7 +2,7 @@
 cd $AGL_TOP
 
 # build setup
-source meta-agl/scripts/aglsetup.sh -f -m raspberrypi4 -b raspberrypi4 agl-demo agl-appfw-smack
+source meta-agl/scripts/aglsetup.sh -f -m raspberrypi4 -b raspberrypi4 agl-demo
 
 # if want to customise, please change /build/conf/local.conf file
 echo 'DL_DIR ?= "/home/dev/share/agl/downloads"' >> $AGL_TOP/raspberrypi4/conf/local.conf
