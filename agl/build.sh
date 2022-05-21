772efec092fbd97ebc2151720d1c92993006bd88
Takeshi Fukui <yankeerunner@gmail.com>
2022-05-21 12:16:09 +0900

    add SSTATE_DIR, DL_DIR

--- agl/build.sh
+++ agl/build.sh
@@ -1,11 +1,12 @@
 # setup build environment
-source $HOME/.bashrc
 cd $AGL_TOP/koi
 
 # build setup
 source meta-agl/scripts/aglsetup.sh -f -m raspberrypi4 -b raspberrypi4 agl-demo agl-appfw-smack
 
 # if want to customise, please change /build/conf/local.conf file
+echo 'DL_DIR ?= "/home/dev/share/agl/downloads"' >> $AGL_TOP/koi/raspberrypi4/conf/local.conf
+echo 'SSTATE_DIR ?= "/home/dev/share/agl/sstate-cache"' >> $AGL_TOP/koi/raspberrypi4/conf/local.conf
 
 # build agl-demo-platform
 time bitbake agl-demo-platform
