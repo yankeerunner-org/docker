e40532e160b7bb132e5bdb00e4a89e6bab55b7c2
Takeshi Fukui <yankeerunner@gmail.com>
2023-11-04 21:30:47 +0900

    ビルドスクリプト修正

--- agl/user/start_build.sh
+++ agl/user/start_build.sh
@@ -1,3 +1,9 @@
 SCRIPT_DIR="$(cd $(dirname $0); pwd)"
 source ${SCRIPT_DIR}/.env
-docker run --name ${CONTAINER_NAME} -it ghcr.io/yankeerunner-org/agl:dunfell /bin/bash
+if [ "$(docker ps -a | grep ${CONTAINER_NAME})" ] ; then
+  echo "found container. remove it. name=${CONTAINER_NAME}"
+  docker rm ${CONTAINER_NAME} -f
+else
+  echo "not found container. ignore it. name=${CONTAINER_NAME}"
+fi
+docker run --name ${CONTAINER_NAME} -it ghcr.io/yankeerunner-org/agl:dunfell /bin/bash -i -c "/home/dev/build.sh && /bin/bash"
