128037d3cc8a1ed09ac95da1fee72a94a8bc0c90
Takeshi Fukui <yankeerunner@gmail.com>
2023-11-04 14:37:49 +0900

    リファクタリング

--- agl/setup.sh
+++ agl/setup.sh
@@ -15,17 +15,3 @@ chmod a+x $HOME/bin/repo
 cd $AGL_TOP
 python3 $HOME/bin/repo init -b marlin -m marlin_13.0.0.xml -u https://gerrit.automotivelinux.org/gerrit/AGL/AGL-repo
 python3 $HOME/bin/repo sync
-
-# setup build environment
-cd $AGL_TOP
-
-# build setup
-source meta-agl/scripts/aglsetup.sh -f -m raspberrypi4 -b raspberrypi4 agl-demo
-
-# if want to customise, please change /build/conf/local.conf file
-echo 'DL_DIR ?= "/home/dev/share/agl/downloads"' >> $AGL_TOP/raspberrypi4/conf/local.conf
-echo 'SSTATE_DIR ?= "/home/dev/share/agl/sstate-cache"' >> $AGL_TOP/raspberrypi4/conf/local.conf
-
-# build agl-demo-platform
-time bitbake agl-demo-platform
-cp -R $AGL_TOP/raspberrypi4/tmp/deploy/images/raspberrypi4-64 /home/dev/share/agl
