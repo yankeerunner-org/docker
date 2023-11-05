3b61de5015254759628ffff51e6549baa19dd388
Takeshi Fukui <yankeerunner@gmail.com>
2023-11-05 16:03:44 +0900

    image名更新

--- agl/user/get_image.sh
+++ agl/user/get_image.sh
@@ -1,3 +1,3 @@
 SCRIPT_DIR="$(cd $(dirname $0); pwd)"
 source ${SCRIPT_DIR}/.env
-docker cp -L ${CONTAINER_NAME}:/home/dev/AGL/raspberrypi4/tmp/deploy/images/raspberrypi4-64/agl-demo-platform-raspberrypi4-64.wic.xz .
+docker cp -L ${CONTAINER_NAME}:/home/dev/AGL/raspberrypi4/tmp/deploy/images/raspberrypi4-64/agl-ivi-demo-platform-flutter-raspberrypi4-64.wic.xz .
